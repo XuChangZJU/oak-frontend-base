b3b53f93ceb4d2e88674635fd236547182266620
梁朝伟 <2211960668@qq.com>
2022-04-29 14:08:43 +0800

    fix polyfill.ts

---
+++ src/features/index.d.ts
@@ -0,0 +1,11 @@
+import { Aspect, Checker, Context, EntityDict, RowStore } from 'oak-domain/lib/types';
+import { Cache } from './cache';
+import { Location } from './location';
+import { RunningNode } from './node';
+import { StorageSchema } from 'oak-domain/lib/types/Storage';
+export declare function initialize<ED extends EntityDict, Cxt extends Context<ED>, AD extends Record<string, Aspect<ED, Cxt>>>(storageSchema: StorageSchema<ED>, createContext: (store: RowStore<ED, Cxt>) => Cxt, checkers?: Array<Checker<ED, keyof ED, Cxt>>): BasicFeatures<ED, Cxt, AD>;
+export declare type BasicFeatures<ED extends EntityDict, Cxt extends Context<ED>, AD extends Record<string, Aspect<ED, Cxt>>> = {
+    cache: Cache<ED, Cxt, AD>;
+    location: Location;
+    runningNode: RunningNode<ED, Cxt, AD>;
+};
