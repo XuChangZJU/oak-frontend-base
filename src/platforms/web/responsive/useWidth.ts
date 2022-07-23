fdc9d48f7079890c71d6c94a1e4023122d218305
Xc <chinawraith@163.com>
2022-07-23 14:30:54 +0800

    merge

--- src/platforms/web/responsive/useWidth.ts
+++ src/platforms/web/responsive/useWidth.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import { useContext, useState, useEffect } from 'react';
-=======
 import { useContext } from 'react';
->>>>>>> 7c9ee6c720b0be3cc9e4862427d3c19aca378add
 import { useMediaQuery } from 'react-responsive';
 
 import {
