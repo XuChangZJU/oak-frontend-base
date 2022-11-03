fc166f0ab7ae4d1075fc053ba0f53f5940c09253
Xc@centOs <chinawraith@163.com>
2022-11-03 10:27:48 +0800

    SingleNode在addOperation过程中的小问题

--- src/features/runningTree.ts
+++ src/features/runningTree.ts
@@ -1254,7 +1254,6 @@ class SingleNode<ED extends EntityDict & BaseEntityDict,
         };
         if (this.operations.length === 0) {
             // 处理一下create
-            this.operations.push(operation as Operation<ED, T>);
             if (oper.action === 'create') {
                 const id = await generateNewId();
                 Object.assign(oper.data, {
@@ -1262,6 +1261,7 @@ class SingleNode<ED extends EntityDict & BaseEntityDict,
                 });
             }
             Object.assign(oper, { id: await generateNewId() });
+            this.operations.push(operation as Operation<ED, T>);
         }
         else {
             // singleNode上应当有且只有一个operation，无论什么情况
