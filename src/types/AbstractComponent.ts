628d16c5d5e06e3fcd6fd55206e742a5b7f42d54
wangwenchen <920960949@qq.com>
2023-03-10 16:59:33 +0800

    attr

--- src/types/AbstractComponent.ts
+++ src/types/AbstractComponent.ts
@@ -28,7 +28,6 @@ export type OakAbsDerivedAttrDef = {
     type?: 'img' | 'file' | 'avatar';
 };
 
-
 export type OakAbsAttrDef = string | OakAbsDerivedAttrDef;
 
 export type OakAbsAttrDef_Mobile = {
@@ -37,26 +36,34 @@ export type OakAbsAttrDef_Mobile = {
     rowsPath: OakAbsAttrDef[];
 };
 
-export interface OakAbsRefAttrPickerDef<ED extends EntityDict & BaseEntityDict, T extends keyof ED> {
+export interface OakAbsRefAttrPickerDef<
+    ED extends EntityDict & BaseEntityDict,
+    T extends keyof ED
+> {
     mode: 'select' | 'list' | 'radio';
     attr: string;
     entity: T;
     projection: ED[T]['Selection']['data'] | (() => ED[T]['Selection']['data']);
     title: (row: ED[T]['Schema']) => string;
-    filter?: ED[T]['Selection']['filter'] | (() => ED[T]['Selection']['filter']);
+    filter?:
+        | ED[T]['Selection']['filter']
+        | (() => ED[T]['Selection']['filter']);
     count?: number;
     label?: string;
     allowNull?: boolean;
-};
+}
 
-export type OakAbsRefAttrPickerRender<ED extends EntityDict & BaseEntityDict, T extends keyof ED> = {
+export type OakAbsRefAttrPickerRender<
+    ED extends EntityDict & BaseEntityDict,
+    T extends keyof ED
+> = {
     type: 'ref';
     attr: string;
     label: string;
     value: any;
     required?: boolean;
-    mode: OakAbsRefAttrPickerDef<ED, T>['mode'],
-}
+    mode: OakAbsRefAttrPickerDef<ED, T>['mode'];
+};
 
 export type OakAbsGeoAttrsDef = {
     amapSecurityJsCode: string;
@@ -69,15 +76,21 @@ export type OakAbsGeoAttrsDef = {
     };
 };
 
-export type OakAbsAttrUpsertDef<ED extends EntityDict & BaseEntityDict> = OakAbsRefAttrPickerDef<ED, keyof ED> | string;
+export type OakAbsAttrUpsertDef<ED extends EntityDict & BaseEntityDict> =
+    | OakAbsRefAttrPickerDef<ED, keyof ED>
+    | string;
 
-import { DataType, DataTypeParams } from 'oak-domain/lib/types/schema/DataTypes';
+import {
+    DataType,
+    DataTypeParams,
+} from 'oak-domain/lib/types/schema/DataTypes';
 export type AttrRender = {
     label: string;
     value: any;
     type: DataType | ('img' | 'file' | 'avatar' | 'ref');
     color?: string;
     width?: RenderWidth;
+    attr: string;
 };
 
 export type OakNativeAttrUpsertRender = {
@@ -91,7 +104,9 @@ export type OakNativeAttrUpsertRender = {
     enumeration?: Array<{ label: string; value: string }>;
 };
 
-export type AttrUpsertRender<ED extends EntityDict & BaseEntityDict> = OakNativeAttrUpsertRender | OakAbsRefAttrPickerRender<ED, keyof ED>;
+export type AttrUpsertRender<ED extends EntityDict & BaseEntityDict> =
+    | OakNativeAttrUpsertRender
+    | OakAbsRefAttrPickerRender<ED, keyof ED>;
 
 export type ColumnDefProps = {
     width: number;
@@ -105,6 +120,8 @@ export type ColumnDefProps = {
 
 export type DataTransformer = (data: object) => AttrRender[];
 
-export type DataUpsertTransformer<ED extends EntityDict & BaseEntityDict> = (data: object) => AttrUpsertRender<ED>[];
+export type DataUpsertTransformer<ED extends EntityDict & BaseEntityDict> = (
+    data: object
+) => AttrUpsertRender<ED>[];
 
 export type DataConverter = (data: any[]) => Record<string, any>;
