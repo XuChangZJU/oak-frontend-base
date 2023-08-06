aed84c6f16b228b3a558aa4a4d3c8b412bd5d361
Xc@centOs <chinawraith@163.com>
2023-08-06 15:01:59 +0800

    增加了debugStore.exec方法,直接抛异常

--- lib/debugStore/DebugStore.d.ts
+++ lib/debugStore/DebugStore.d.ts
@@ -13,6 +13,7 @@ export declare class DebugStore<ED extends EntityDict & BaseEntityDict, Cxt exte
     private executor;
     private relationAuth;
     constructor(storageSchema: StorageSchema<ED>, contextBuilder: (cxtString?: string) => (store: DebugStore<ED, Cxt>) => Promise<Cxt>, actionCascadeGraph: AuthCascadePath<ED>[], relationCascadeGraph: AuthCascadePath<ED>[], authDeduceRelationMap: AuthDeduceRelationMap<ED>, selectFreeEntities: (keyof ED)[]);
+    exec(script: string, txnId?: string): Promise<void>;
     aggregate<T extends keyof ED, OP extends SelectOption>(entity: T, aggregation: ED[T]["Aggregation"], context: Cxt, option: OP): Promise<AggregationResult<ED[T]["Schema"]>>;
     begin(option?: TxnOption): Promise<string>;
     commit(txnId: string): Promise<void>;
