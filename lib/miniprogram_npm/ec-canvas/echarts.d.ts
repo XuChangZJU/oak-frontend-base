84bae06b98efb6aec1df6caa9bdcf7210f49fa65
梁朝伟 <2211960668@qq.com>
2023-08-10 14:55:36 +0800

    小程序组件ec-canvas声明文件文件类型为ts

---
+++ lib/miniprogram_npm/ec-canvas/echarts.d.ts
@@ -0,0 +1,7 @@
+declare type Options = {
+    width?: number;
+    height?: number;
+    devicePixelRatio?: number;
+};
+export declare function init(dom: any, theme?: string | null, options?: Options): any;
+export {};
