3df45090a6f6afe08dca1740b8310c148e81e3bf
wkj <278599135@qq.com>
2023-11-10 12:22:19 +0800

    移除 lib下miniprogram_npm

--- lib/miniprogram_npm/ec-canvas/echarts.d.ts
+++
@@ -1,7 +0,0 @@
-type Options = {
-    width?: number;
-    height?: number;
-    devicePixelRatio?: number;
-};
-export declare function init(dom: any, theme?: string | null, options?: Options): any;
-export {};
